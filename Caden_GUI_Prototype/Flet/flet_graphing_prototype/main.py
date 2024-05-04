780a2f952d5ce799598f5e44b3f2c0992c8e390e
Caden Tedeschi <cmt489@nau.edu>
2024-05-03 21:42:34 -0700

    Updating graph prototype

--- Caden_GUI_Prototype/Flet/flet_graphing_prototype/main.py
+++ Caden_GUI_Prototype/Flet/flet_graphing_prototype/main.py
@@ -7,17 +7,39 @@ import matplotlib.pyplot as plt
 def main(page: ft.Page):
 
     def add_point(event):
+        x_int_val = None
+        y_int_val = None
+
+        try:
+            x_int_val = int(x_entry.value)
+        except ValueError:
+            x_entry.value = None
+            page.snack_bar = ft.SnackBar(ft.Text("Please enter a number"))
+            page.snack_bar.duration = 1500
+            page.snack_bar.open = True
+
+            page.update()
+
+        try:
+            y_int_val = int(y_entry.value)
+        except ValueError:
+            y_entry.value = None
+            page.snack_bar = ft.SnackBar(ft.Text("Please enter a number"))
+            page.snack_bar.duration = 1500
+            page.snack_bar.open = True
+            page.update()
+
         if x_entry.value == '' or y_entry.value == '':
             x_entry.value = '0'
             y_entry.value = '0'
-        x_points.append(int(x_entry.value))
-        y_points.append(int(y_entry.value))
+        x_points.append(x_int_val)
+        y_points.append(y_int_val)
         ax.plot(x_points, y_points, plot_type)
         page.update()
 
     x_points = [1]
     y_points = [1]
-    plot_type = 'ro'
+    plot_type = 'bo'
 
     fig, ax = plt.subplots()
 
