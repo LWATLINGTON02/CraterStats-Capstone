c30d04552266e326e7e3453c1bb552002b88c060
Ibrahim Hmood <ih295@nau.edu>
2024-12-10 12:31:02 -0700

    Fixed pip install issue
    There was an issue where craterstats-gui could not find where the Alpha folder is, this fixed that.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="Alpha",                    # Project name
+    name="craterstats_gui",                    # Project name
     version="1.0.0",                      # Version
     author="Lunar Pit Patrol",
     author_email="EPalmisanoConnect@gmail.com",
